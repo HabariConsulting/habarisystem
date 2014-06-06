8726f85a3574e5de44823a76bbc1d2cd815cc384
Ligono Eugene <ligonoeugene@gmail.com>
2014-06-06 13:35:30 +0300

    config change
    base url change

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/habari_time/';
+$config['base_url']	= 'http://localhost/habarisystem/';
 
 /*
 |--------------------------------------------------------------------------
